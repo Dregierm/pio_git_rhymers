3517c71d5e5ad705256d072efba848beead52c16
Dregierm <235856@edu.p.lodz.pl>
2022-04-28 16:03:56 +0200

    16. Dodanie dokumentacji do klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,19 +1,29 @@
 package edu.kis.vh.nursery;
 
+
+/**
+ * Klasa FIFORhymer dziedziczy po klasie DefaultCountingOutRhymer
+ * Nadpisuje metode countOut
+ * Posiada jedno pole typu DefaultCountingOutRhymer
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
 	private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-	
+
+	/**
+	 * Metoda countOut zwraca i usuwa pierwszy element z tablicy numbers (int)
+	 * lub -1 w przypadku pustej tablicy
+	 */
 	@Override
 	public int countOut() {
 		while (!callCheck())
-			
-		temp.countIn(super.countOut());
+
+		temp.countIn(super.countOut());  
 		
 		int ret = temp.countOut();
 		
 		while (!temp.callCheck())
-			
+
 		countIn(temp.countOut());
 		
 		return ret;
